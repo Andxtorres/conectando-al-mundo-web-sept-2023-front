3027c60a8b3fcadda5cea709ea711e3c35e6fa01
Andrés Fernando Torres Morán <atorres@applab.mx>
2023-09-14 12:45:47 -0600

    Connetion

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 import Example from './components/Example';
 import Pokedex from './components/Pokedex';
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import CreateUserForm from './screens/CreateUserForm';
 
+const router = createBrowserRouter([
+  {
+    path: "/pokedex",
+    element: (
+      <Pokedex></Pokedex>
+    ),
+  },
+  {
+    path: "/user-form",
+    element: <CreateUserForm></CreateUserForm>,
+  },
+]);
 function App() {
   return (
     <div className="App">
-      <Pokedex></Pokedex>
+      <React.StrictMode>
+        <RouterProvider router={router} />
+      </React.StrictMode>
     </div>
   );
 }
